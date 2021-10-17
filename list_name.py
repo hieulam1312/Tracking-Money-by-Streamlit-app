a9b8c4abea9a63543a6f08c64907aa7de1e2d94f
hieulam1312 <hieulam1312@gmail.com>
2021-10-17 20:27:24 +0700

    Create list_name.py

---
+++ list_name.py
@@ -0,0 +1,19 @@
+# Các nhóm thu chi chính
+A=['Thu','Chi']
+T=['Lương','Thưởng','Bất ngờ','Lợi nhuận đầu tư']
+C=['Sinh hoạt','Kiến thức','Trao tặng','Bất ngờ','TS cố định']
+
+#Danh sách các nhóm chi tiêu theo từng nhóm hạng mục
+sh=['Ăn','Xăng','Nhà','Dụng cụ','Nhà','Điện','Nước','Sim','Wifi']
+kt=['Sách','Khóa học','Bạn bè','VPP','Khác']
+tt=['Trồng rừng','Người nghèo','Tặng khóa học']
+bg=['Mua lại đồ bị mất/hư','Bệnh/Sức khỏe','Cưới xin/Ma chay','Sửa xe','Khác']
+ts_cđ=['Thiết bị điện tử','Vật dụng nhà']
+_list_c=[sh,kt,tt,bg,ts_cđ]
+
+#Danh sách các nhóm thu nhập theo từng hạng mục
+l1=['Lương hàng tháng','Lương dự án']
+t=['Thưởng tháng','Thưởng nóng','Tết','Lương 13']
+bg=['Được cho tặng']
+ln=['Tiết kiệm','Chứng khoán','Coin','App/Tool','Youtube']
+_list_t=[l1,t,bg,ln]
